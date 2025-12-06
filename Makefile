9a94d5ba444ea284c481faab1850e351c1513aa0
Fernando Iazeolla <fernando.iazeolla@gmail.com>
2025-12-06 01:09:52 +0100

    added double linked list example.

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+ARGS="-lc -lm"
+
 main: main.rs
-	rustc --edition 2021 -g -C link-args="-lc -lm" -C opt-level=z -C panic="abort" main.rs -o main
+	rustc --edition 2021 -g -C link-args=$(ARGS) -C opt-level=z -C panic="abort" main.rs -o main
+
--- main.rs
+++ main.rs
@@ -4,7 +4,7 @@
 use core::ffi::{c_int, c_void};
 use core::panic::PanicInfo;
 use core::ptr;
-use core::mem::zeroed;
+use core::mem::{zeroed, size_of};
 
 #[panic_handler]
 unsafe fn panic(_info:&PanicInfo) -> ! {
@@ -18,9 +18,18 @@ pub struct Array<T> {
     capacity: usize,
     list: *mut T,
 }
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct List<T> {
+    prev: *mut List<T>,
+    next: *mut List<T>,
+    item: T,
+}
 extern "C" {
     fn printf(fmt: *const u8, ...) -> c_int;
     fn realloc(prt: *mut c_void, size: usize) -> *mut c_void;
+    fn malloc(size: usize) -> *mut c_void;
     // fn exit(code:i32)-> !;
 }
 
@@ -37,20 +46,53 @@ unsafe fn array_push<T>(l: *mut Array<T>, item: T) {
     (*l).count+=1;
 }
 
+unsafe fn list_push<T: Copy>(first: &mut *mut List<T>, last: &mut *mut List<T>, item: T) {
+    let new_node = malloc(size_of::<List<T>>()) as *mut List<T>;
+    if new_node.is_null() {
+        return;
+    }
+    (*new_node).item = item;
+    (*new_node).next = ptr::null_mut();
+    (*new_node).prev = *last;
+
+    if (*last).is_null() {
+        *first = new_node;
+        *last = new_node;
+    } else {
+        (**last).next = new_node;
+        *last = new_node;
+    }
+}
+
 #[no_mangle]
 unsafe extern "C" fn main(argc:i32, argv:*mut *mut u8) -> i32 {
    printf(b"foo %d\n\0".as_ptr(), argc);
    for i in 0 .. argc {
-       printf(b"%s\n".as_ptr(), *argv.add(i as usize));
+       printf(b"%s\n\0".as_ptr(), *argv.add(i as usize));
    }
    let mut l:Array<i32>=zeroed();
    for i in 0 .. 100 {
        array_push(&mut l, i);
    }
    for i in 0 .. l.count {
-       printf(b"%d ".as_ptr(), *(l.list.add(i as usize)));
+       printf(b"%d \0".as_ptr(), *(l.list.add(i as usize)));
    }
-   // exit(48);
+   
+   printf(b"\n-- Double Linked List Demo --\n\0".as_ptr());
+   let mut first: *mut List<i32> = ptr::null_mut();
+   let mut last: *mut List<i32> = ptr::null_mut();
+   
+   list_push(&mut first, &mut last, 32);
+   list_push(&mut first, &mut last, 64);
+   list_push(&mut first, &mut last, 128);
+
+   let mut current = first;
+   while !current.is_null() {
+       printf(b"%d \0".as_ptr(), (*current).item);
+       current = (*current).next;
+   }
+   printf(b"\n\0".as_ptr());
+   
    48
 }
 
